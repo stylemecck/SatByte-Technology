d336cca1920faa77ea595e5f32da5966770ad2b4
stylemecck <satyamkashyapoffical@gmail.com>
2026-04-18 12:35:07 +0530

    feat: configure MainActivity to request the maximum hardware refresh rate on Android R+

--- android/app/src/main/java/com/satbyte/app/MainActivity.java
+++ android/app/src/main/java/com/satbyte/app/MainActivity.java
@@ -2,4 +2,37 @@ package com.satbyte.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Push the refresh rate to the maximum supported by the hardware (90Hz/120Hz)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Display display = getDisplay();
+            if (display != null) {
+                Display.Mode[] modes = display.getSupportedModes();
+                Display.Mode highestMode = null;
+                float maxRate = 0;
+                
+                for (Display.Mode mode : modes) {
+                    if (mode.getRefreshRate() > maxRate) {
+                        maxRate = mode.getRefreshRate();
+                        highestMode = mode;
+                    }
+                }
+                
+                if (highestMode != null) {
+                    WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+                    layoutParams.preferredDisplayModeId = highestMode.getModeId();
+                    getWindow().setAttributes(layoutParams);
+                }
+            }
+        }
+    }
+}
